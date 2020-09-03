dba5b52c57cf7bd01245fa0fc8a7f778bbffaf54
Matheus Gonçalves <matheusgfgl@gmail.com>
2020-09-03 17:31:34 -0300

    Adding Api call

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard 
+      v-for="(event, index ) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '../components/eventCard.vue'
 export default {
+  components: {
+    EventCard
+  },
+
   head() {
     return {
       title: 'Event Listing '
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events, try again'
+        })
+      })
   }
 }
 </script>
